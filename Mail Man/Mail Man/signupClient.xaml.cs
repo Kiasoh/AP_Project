4586b39f9801e6b09e0ae45d9eb83417a06bb8ad
sobhan2023 <sohrab2020s@gmail.com>
2023-06-03 14:53:12 +0330

    fix some bug of UI

---
+++ Mail Man/Mail Man/signupClient.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Mail_Man
+{
+    /// <summary>
+    /// Interaction logic for signupClient.xaml
+    /// </summary>
+    public partial class signupClient : Window
+    {
+        public signupClient()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSignUp_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void tbName_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void tbID_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void tbEmail_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
